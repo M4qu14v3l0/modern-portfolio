cd9bc04d84cba797d7e7d99eb45536d2bf86094f
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-01-29 15:07:53 -0500

    adding social

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,21 @@
+'use client'
+
+import Love from "./portada/Love";
 import Portada from "./portada/Portada";
+import Presentation from "./portada/Presentation";
+import Social from "./portada/Social";
+import Work from "./portada/Work";
 
 export default function HomePage(){
     return(
         <main className="home-container">
+            <div className="portada-container">
             <Portada />
-            
+            <Presentation />
+            <Work />
+            <Love />
+            <Social />
+            </div>
         </main>
     )
 }
---
+++ app/portada/Social.jsx
@@ -0,0 +1,33 @@
+//  Material Icons
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import SendIcon from '@mui/icons-material/Send';
+import Link from 'next/link';
+
+export default function Social () {
+
+    const social = [
+        {ico: LinkedInIcon, label:'@marcelo', url:'https://www.linkedin.com/in/marcelo-llerena-paricahua-20604323a'},
+        {ico: InstagramIcon, label:'@marcelo_coding', url:'https://www.instagram.com/marcelo_coding/'},
+        {ico: GitHubIcon, label:'@m4qu14v3l0', url:'https://github.com/M4qu14v3l0'},
+        {ico: SendIcon, label:'@marcelo', url:'mailto:marcelo.gabriel.llerena@gmail.com'},
+
+    ]
+
+    return(
+        <section>
+            <h3>On the web</h3>
+
+
+            {
+                social.map( item => (
+                    <Link href={item.url} key={item.label} target="_blank" className='social-ico-item'>
+                        {<item.ico />}
+                        {item.label}
+                    </Link>
+                ))
+            }
+        </section>
+    )
+}
