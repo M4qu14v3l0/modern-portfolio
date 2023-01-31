000790e2a617db9d0bbdbfd2809266fa28190e94
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-01-31 14:19:21 -0500

    final change on love

--- app/portadaComponents/Love.jsx
+++ app/portadaComponents/Love.jsx
@@ -2,7 +2,7 @@ export default function () {
     return(
         <section>
             <h3>I ❤</h3>
-            <p>Programming on cafe store, listening music especially lo-fi, working with react and learning more about it, hang out with my gf, learn about errors</p>
+            <p>Programming on cafe store, listening music especially lo-fi, working with react and learning more about it, hang out with my gf, learn of mistakes</p>
         </section>
     )
 }
