a01d085569ed056974370cba875a3663b491c459
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-03-01 22:32:20 -0500

    comment

--- app/works/page.jsx
+++ app/works/page.jsx
@@ -14,7 +14,7 @@ export default function Works () {
                 <Collaboration />
                 <OldWorks />
             </Container>
-            {/* commen*/}
+            {/* comme*/}
         </main>
     )
 }
